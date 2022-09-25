93746b8b20fabba33108c488dfa74e5d0d73174a
katleho <katlehomofokeng@protonmail.com>
2022-09-25 11:03:04 +0200

    added Employee entity

---
+++ backend/src/main/java/com/example/backend/entity/Employee.java
@@ -0,0 +1,49 @@
+package com.example.backend.entity;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    public  Employee(){}
+
+    public Employee(String firstName, String lastName, String emailAddress) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
