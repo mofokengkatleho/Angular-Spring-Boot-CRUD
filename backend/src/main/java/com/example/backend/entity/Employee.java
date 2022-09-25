4c37e6c2b6a217610d14dc8974b15697be24b6ec
katleho <katlehomofokeng@protonmail.com>
2022-09-25 11:05:43 +0200

    added JPA annotation to Employee

--- backend/src/main/java/com/example/backend/entity/Employee.java
+++ backend/src/main/java/com/example/backend/entity/Employee.java
@@ -1,9 +1,18 @@
 package com.example.backend.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email_address")
     private String emailAddress;
 
     public  Employee(){}
