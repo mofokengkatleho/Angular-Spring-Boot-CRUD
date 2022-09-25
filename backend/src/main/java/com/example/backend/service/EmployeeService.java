349411c924bd5260ef7b32647ce05d8483ef6d75
katleho <katlehomofokeng@protonmail.com>
2022-09-25 13:08:36 +0200

    added EmployeeService

---
+++ backend/src/main/java/com/example/backend/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.backend.service;
+
+import com.example.backend.entity.Employee;
+
+import java.util.List;
+
+interface EmployeeService {
+    public List<Employee> getEmployees();
+}
